e4484f10332f68ca00f7b57b56cdf2d9771fa817
pramodagoudapatil <pramodnpatil02@gmail.com>
2026-01-16 13:38:00 +0530

    backend deployed on render

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:9090', // Your backend URL
+        target: 'https://salessavvyproject.onrender.com', // Your backend URL
         changeOrigin: true,
       },
     },
