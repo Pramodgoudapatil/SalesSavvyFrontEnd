77ea2d1f7537f8001aec0b426351664619b4830b
pramodagoudapatil <pramodnpatil02@gmail.com>
2026-02-18 00:57:54 +0530

    back to same before link

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -19,7 +19,7 @@ export default function LoginPage() {
     }
 
     try {
-       const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/api/auth/login`, {
+       const response = await fetch("http://localhost:9090/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
