32f6f7d765ef1a1841b6e5e29a0890e6b844c2ed
pramodagoudapatil <pramodnpatil02@gmail.com>
2026-02-17 20:25:32 +0530

    backend link changed

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./assets/styles.css";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export default function LoginPage() {
   const [username, setUsername] = useState("");
@@ -19,7 +20,7 @@ export default function LoginPage() {
 
     try {
       // const response = await fetch("http://localhost:9090/api/auth/login", {
-      const response = await fetch("https://salessavvyproject.onrender.com/api/auth/login", {
+      const response = await fetch(`${BASE_URL}/api/auth/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
